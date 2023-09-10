f47e14a89161199fd6f6376d9ca034c89df1f8ea
Shehzad Iqbal <shehzaddiqbal@gmail.com>
2023-09-11 01:38:17 +0500

    Update getEmbeddings.ts

--- src/helpers/getEmbeddings.ts
+++ src/helpers/getEmbeddings.ts
@@ -1,28 +1,46 @@
 import OpenAI from "openai";
 import { OPEN_AI_KEY } from "../config/index.mjs";
+import { cleanText } from "./textCleaning.js";
 
-/*
-add js docs
-@ getEmbeddings return vector
-*/
+/**
+ * Get text embeddings using the OpenAI API.
+ * @param text - The input text for which embeddings are required.
+ * @returns Promise<{vector: number[]}> - An containing the text embeddings and API usage details.
+ * @throws Error if there's an issue with the API call or response.
+ * @description
+ * Embeddings
+ * @async
+ * converts text into vector
+ * @returns A 1536 dimension vector number[]
+ */
+
 export const getEmbeddings = async (text: string) => {
   try {
-    const openai = new OpenAI({
-      apiKey: OPEN_AI_KEY,
-    });
+    if (text.length > 250) throw new Error("Text is too long!!");
+    text = cleanText(text);
+
+    const openai = new OpenAI({ apiKey: OPEN_AI_KEY });
 
     const response = await openai.embeddings.create({
       model: "text-embedding-ada-002",
       input: text,
     });
 
-    const vector = response?.data[0]?.embedding;
+    console.log("🚀 ~ getEmbeddings.ts:18 response:", response);
 
-    return vector;
+    if (!response?.data || response?.data?.length === 0)
+      throw new Error("No embeddings data found in the API response.");
+
+    const vector = response.data[0].embedding;
+
+    return { vector, usage: response.usage };
   } catch (err: any) {
-    console.log("🚀 ~ file: index.ts:7 ~ getVectorByEmbeddings ~ err:", err);
-    throw Error(
-      err.message || "Error occurred while creating embeddings from OpenAI"
+    console.error("🚀 ~ file: index.ts:7 ~ getVectorByEmbeddings ~ err:", err);
+    throw new Error(
+      err.message || "An error occurred while creating embeddings from OpenAI."
     );
   }
 };
+
+// example to call this function
+// const { vector } = await getEmbeddings("");
